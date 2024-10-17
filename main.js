ae2f7b117a9ca463dea1e7ee28c9a9ba8edff781
saidmuhamedovsaidakbar27@gmail.com <Brat711>
2024-10-17 20:48:13 +0500

    bro

---
+++ main.js
@@ -0,0 +1,14 @@
+var username = prompt("Ismingizni kiriting")
+var Familiyangizni = prompt("Familiyangizni kiriting")
+var Yoshingizni = prompt("Yoshingizni Kiriting")
+var faningiz = prompt("Yoqtirgan faningiz yozing ")
+var Yashaydigan = prompt("Yashaydigan tumaningizni kiriting")
+console.log(`
+ Ism: ${username}
+ Familiya: ${Familiyangizni}
+ Yosh: ${Yoshingizni}
+ Yoqtirgan fan: ${faningiz}
+ Yashash manzil: ${Yashaydigan}
+  `);
+
+
